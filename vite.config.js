6e9aef64c07efd3613a4155e27e0fcb9415e7ca2
German Gonzalez <germanngc@gmail.com>
2024-05-29 11:07:15 -0600

    fix(livewire.js) Fixing livewire js minified version

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,19 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/auth.css',
+            input: ['resources/css/admin.css',
+                'resources/css/auth.css',
                 'resources/css/app.css',
-                'resources/js/app.js',
+                'resources/js/admin.js',
                 'resources/js/auth.js',
+                'resources/js/app.js',
                 'resources/js/public/index.js',
                 'resources/js/public/contact.js',
                 'resources/js/public/pricing.js'],
             refresh: true,
         }),
     ],
+    build: {
+        minify: 'terser'
+    }
 });
