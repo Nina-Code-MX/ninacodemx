1b34d5caa30e42ee9959030037afdd1d43a5bfd6
Max Mancera <manceragarciamax@gmail.com>
2024-06-08 18:23:22 -0600

    Cambio en paqueteria softdeletes

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,12 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes as EloquentSoftDeletes;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 
 class Article extends Model
 {
-    use HasFactory, EloquentSoftDeletes;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
 
